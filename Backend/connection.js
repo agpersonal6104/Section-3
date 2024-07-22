1fbd48fa395e87e4efd697a57458ef8bfb83838d
AyushG <legendsteamprivate@gmail.com>
2024-07-22 19:20:45 +0530

    Backend

--- Backend/connection.js
+++ Backend/connection.js
@@ -3,4 +3,9 @@ const mongoose = require('mongoose');
 const url = "mongodb+srv://agpersonal0614:AGcluster0@cluster0.gjcbjde.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
 
 // asynchronous function  promise
-mongoose.connect(url);
+mongoose.connect(url)
+.then((result) => {
+    console.log('DataBase connected');   
+}).catch((err) => {
+    console.log(err);
+});
