7350669cb52c86d833510b921a711190a85bf6da
AyushG <legendsteamprivate@gmail.com>
2024-08-07 19:15:07 +0530

    Backend start

--- Backend/routers/UserRouter.js
+++ Backend/routers/UserRouter.js
@@ -7,9 +7,25 @@ router.get('/add', (req,res) => {
 });
 
 // getall
+router.get('/getall', (req,res) => {
+    res.send('Response from user getall');
+});
+
 // getbyid
+router.get('/getbyid', (req,res) => {
+    res.send('Reponse from User ID');
+});
+
 // update
+router.get('/update',(req,res) => {
+    res.send('Server Updated')
+});
+
 // delete
+
+router.get('/delete',(req, res) => {
+    res.get('deleted')
+});
 
 // npm init -y
 // npm i express
