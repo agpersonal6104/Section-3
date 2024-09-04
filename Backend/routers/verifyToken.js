bb2e601072ce2f344c5198d4b0e72388c9b5edf7
AyushG <legendsteamprivate@gmail.com>
2024-09-04 20:09:20 +0530

    jwt token verification router

--- Backend/routers/verifyToken.js
+++ Backend/routers/verifyToken.js
@@ -5,5 +5,20 @@ require('dotenv').config();
 const verifyToken = (req,res,next) => {
 
     const token = req.headers['x-auth-token'];
-    
+    jwt.verify( token, process.env.JWT_SECRET,
+        (err, payload) => {
+            if(err)
+            {
+                console.log(err);
+                res.status(401).json(err);
+            }
+            else
+            {
+                req.user = payload;
+                next();
+            }
+        }
+     );
 }
+
+module.exports = verifyToken;
